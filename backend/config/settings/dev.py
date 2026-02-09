45685598539e4923c68b0999462f52a298fc7413
aidangilbert <aidancg2@illinois.edu>
2026-02-09 15:08:21 -0600

    feature/added * to server

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -9,7 +9,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 # Development-specific apps
 INSTALLED_APPS += [
