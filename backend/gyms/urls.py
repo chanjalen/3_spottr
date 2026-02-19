f76d94fcfdc1ef899be050f04cb8bb9c571128a9
jalenchan1 <jalen.chan@gmail.com>
2026-02-18 21:42:58 -0600

    tweaks to gyms and social

--- backend/gyms/urls.py
+++ backend/gyms/urls.py
@@ -5,12 +5,7 @@ from . import views
 app_name = 'gyms'
 
 urlpatterns = [
-    # Function-based views
-    path('manual/', views.gym_list_manual, name='gym_list_manual'),
-    path('render/', views.gym_list_render, name='gym_list_render'),
-    # Class-based views
-    path('cbv/', views.GymListView.as_view(), name='gym_list_cbv'),
-    path('generic/', views.GymGenericListView.as_view(), name='gym_list_generic'),
-path('<uuid:pk>/', views.GymDetailView.as_view(), name='gym_detail'),
+    path('', views.GymGenericListView.as_view(), name='gym_list'),
+    path('<uuid:pk>/', views.GymDetailView.as_view(), name='gym_detail'),
     path('<uuid:pk>/api/top-lifters/', views.top_lifters_view, name='top_lifters'),
 ]
