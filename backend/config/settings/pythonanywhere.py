b8347d5f5e96c1e48aaa587f89546add82fb92b6
EverettC22 <evcruzn@gmail.com>
2026-02-27 17:01:34 -0600

    1

--- backend/config/settings/pythonanywhere.py
+++ backend/config/settings/pythonanywhere.py
@@ -6,7 +6,7 @@ import os
 from pathlib import Path
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     os.getenv('PYTHONANYWHERE_DOMAIN', 'aidangilbert.pythonanywhere.com'),
